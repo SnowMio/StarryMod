f766e208fd2c505aed6ea49ac4e56ef096855f02
MinimalMio <yukati2024@outlook.com>
2024-11-09 12:47:06 +0800

    init

---
+++ src/main/java/moe/mmio/StarryDataGenerator.java
@@ -0,0 +1,11 @@
+package moe.mmio;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StarryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

be07aa4b24558fbfac2ffdf7a6baaefae1742db3
MinimalMio <yukati2024@outlook.com>
2024-11-09 16:05:25 +0800

    [textures, models, blocks]: Legend Ore

--- src/main/java/moe/mmio/Starry.java
+++ src/main/java/moe/mmio/Starry.java
@@ -2,6 +2,10 @@ package moe.mmio;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModification;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.AbstractBlock;
@@ -10,10 +14,22 @@ import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class Starry implements ModInitializer {
 	public static final String MOD_ID = "starry";
@@ -24,6 +40,22 @@ public class Starry implements ModInitializer {
 	public static final Item LEGEND_INGOT = new Item(new FabricItemSettings());
 
 	public static final Block STARRY_STONE = new Block(FabricBlockSettings.of(Material.METAL).hardness(1.5f));
+	public static final Block LEGEND_ORE = new Block(FabricBlockSettings.of(Material.METAL).hardness(2.5f));
+
+	private static ConfiguredFeature<?, ?> OVERWORLD_LEGEND_ORE_CONFIGURED = new ConfiguredFeature
+			(Feature.ORE, new OreFeatureConfig(
+					OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
+					Starry.LEGEND_ORE.getDefaultState(),
+					9
+			));
+
+	private static PlacedFeature OVERWORLD_LEGEND_ORE_PLACED = new PlacedFeature
+			(RegistryEntry.of(OVERWORLD_LEGEND_ORE_CONFIGURED),
+					Arrays.asList(
+							CountPlacementModifier.of(3),
+							SquarePlacementModifier.of(),
+							HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64))
+					));
 
 	@Override
 	public void onInitialize() {
@@ -37,5 +69,30 @@ public class Starry implements ModInitializer {
 		Registry.register(Registry.BLOCK, new Identifier("starry", "starry_stone"), STARRY_STONE);
 		Registry.register(Registry.ITEM, new Identifier("starry", "starry_stone"), new BlockItem(STARRY_STONE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
+		Registry.register(Registry.BLOCK, new Identifier("starry", "legend_ore"), LEGEND_ORE);
+		Registry.register(Registry.ITEM, new Identifier("starry", "legend_ore"), new BlockItem(LEGEND_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+
+		LOGGER.info("Starry Mod :: Applying world generation changes :: ORE");
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,
+				new Identifier("starry", "legend_ore_overworld_configured"),
+				OVERWORLD_LEGEND_ORE_CONFIGURED);
+		Registry.register(BuiltinRegistries.PLACED_FEATURE,
+				new Identifier("starry", "legend_ore_overworld_placed"),
+				OVERWORLD_LEGEND_ORE_PLACED);
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES,
+				RegistryKey.of(Registry.PLACED_FEATURE_KEY,
+						new Identifier("starry", "overworld_legend_ore")));
 	}
+
+	// Item group
+	public static final ItemGroup STARRY_GROUP = FabricItemGroupBuilder.create(
+			new Identifier("starry", "starry_item_group"))
+			.icon(() -> new ItemStack(Starry.STARRY_INGOT))
+			.appendItems(stacks -> {
+				stacks.add(new ItemStack(Starry.STARRY_INGOT));
+				stacks.add(new ItemStack(Starry.STARRY_STONE));
+				stacks.add(new ItemStack(Starry.LEGEND_ORE));
+				stacks.add(new ItemStack(Starry.LEGEND_INGOT));
+			})
+			.build();
 }
